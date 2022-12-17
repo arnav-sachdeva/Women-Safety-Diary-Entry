670ab4cd656e731f51951cf507446caf2d3616a5
arnav-sachdeva <66235421+arnav-sachdeva@users.noreply.github.com>
2022-12-18 03:12:29 +0530

    Add files via upload

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const passport = require("passport");
-const CLIENT_HOME_PAGE_URL = "http://localhost:3000";
+const CLIENT_HOME_PAGE_URL = "https://fabulous-cuchufli-b27d82.netlify.app/";
 
 // when login is successful, retrieve user info
 router.get("/login/success", (req, res) => {
