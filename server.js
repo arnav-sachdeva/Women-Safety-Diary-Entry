02f8593fc3c046a1549d3c5cfcf0e4dffa37c14c
arnav-sachdeva <66235421+arnav-sachdeva@users.noreply.github.com>
2022-12-18 03:12:12 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(express.json());
 
 app.use(cors(
   {
-    origin: "http://localhost:3000", // allow to server to accept request from different origin
+    origin: "https://fabulous-cuchufli-b27d82.netlify.app/", // allow to server to accept request from different origin
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true // allow session cookie from browser to pass through
   }
